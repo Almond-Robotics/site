c58eed346626f76ad41673495aa83b472af31f51
tencups <26286782+tencups@users.noreply.github.com>
2020-03-22 15:00:31 -0400

    edited app

--- express/app.js
+++ express/app.js
@@ -3,39 +3,40 @@ var express = require("express");
 var app = express();
 const serverless = require("serverless-http");
 const router = express.Router();
-
-app.use(express.static("public"));
+const root = "C:/Users/Michael/Desktop/almond-bulma/";
 
 router.get("/", function(req, res) {
-    res.sendFile(__dirname + "/src/home/home.html");
+    res.sendFile(root + "/src/home/home.html");
 });
 
 router.get("/about", function(req, res) {
-    res.sendFile(__dirname + "/src/about/about.html");
+    res.sendFile(root + "/src/about/about.html");
 });
 
 router.get("/our-robots", function(req, res) {
-    res.sendFile(__dirname + "/src/robots/our-robots.html");
+    res.sendFile(root + "/src/robots/our-robots.html");
 });
 router.get("/outreach", function(req, res) {
-    res.sendFile(__dirname + "/src/outreach/outreach.html");
+    res.sendFile(root + "/src/outreach/outreach.html");
 });
 
 router.get("/hello-robot", function(req, res) {
-    res.sendFile(__dirname + "/src/hello-robot/hello-robot.html");
+    res.sendFile(root + "/src/hello-robot/hello-robot.html");
 });
 
 router.get("/sponsors", function(req, res) {
-    res.sendFile(__dirname + "/src/sponsors/sponsors.html");
+    res.sendFile(root + "/src/sponsors/sponsors.html");
 });
 router.get("/first-online", function(req, res) {
-    res.sendFile(__dirname + "/src/firstonline/first-online.html");
+    res.sendFile(root + "/src/firstonline/first-online.html");
 });
 router.get("/contact", function(req, res) {
-    res.sendFile(__dirname + "/src/contact/contact.html");
+    res.sendFile(root + "/src/contact/contact.html");
 });
 
 app.listen(3000);
+app.use(express.static("public"));
+app.use("/", express.static("public"));
 app.use("/", router);
 app.use("/.netlify/functions/server", router); // path must route to lambda
 
